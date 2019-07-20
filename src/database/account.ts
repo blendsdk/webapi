fb661fb162cf45bda4556f5ffb37c5086f3e2be0
Gevik Babakhani <gevik@blendjs.com>
2019-07-20 08:07:45 +0200

    Added type annotations

--- src/database/account.ts
+++ src/database/account.ts
@@ -1,9 +1,20 @@
 
-import { sql_query, sql_insert } from "@blendsdk/sqlkit";
+import { sql_query, sql_insert, IDynamicQuery } from "@blendsdk/sqlkit";
 import { ISysUser, ISysRole } from "./dbtypes";
 import bcrypt from "bcryptjs";
+import { wrapInArray } from "@blendsdk/stdlib";
 
-export const deleteUserByUserID = sql_query<ISysUser, { user_id: number }>(
+/**
+ * Interface describing the input parameters of deleteUserByUserID
+ *
+ * @export
+ * @interface IDeleteUserByUserID
+ */
+export interface IDeleteUserByUserID {
+    user_id: number;
+}
+
+export const deleteUserByUserID = sql_query<ISysUser, IDeleteUserByUserID>(
     `delete from sys.user where user_id = :user_id returning *`
     ,
     { single: true }
@@ -22,9 +33,19 @@ export const addUser = sql_insert<ISysUser, ISysUser>("sys.user", {
 });
 
 /**
+ * Interface describing the input parameters of findUserByUsernameOrEmail
+ *
+ * @export
+ * @interface IFindUserByUsernameOrEmail
+ */
+export interface IFindUserByUsernameOrEmail {
+    username: string;
+}
+
+/**
  * Find s user by username or email
  */
-export const findUserByUsernameOrEmail = sql_query<ISysUser, { username: string }>(
+export const findUserByUsernameOrEmail = sql_query<ISysUser, IFindUserByUsernameOrEmail>(
     `
         select
             *
@@ -39,9 +60,19 @@ export const findUserByUsernameOrEmail = sql_query<ISysUser, { username: string 
 
 
 /**
+ * Interface describing the input parameters of getUserRolesByUserID
+ *
+ * @export
+ * @interface IGetUserRolesByUserID
+ */
+export interface IGetUserRolesByUserID {
+    user_id: number;
+}
+
+/**
  * Gets user roles by user ID
  */
-export const getUserRolesByUserID = sql_query<ISysRole[], { user_id: number }>(
+export const getUserRolesByUserID = sql_query<ISysRole[], IGetUserRolesByUserID>(
     `
         select
             r.*
@@ -51,4 +82,28 @@ export const getUserRolesByUserID = sql_query<ISysRole[], { user_id: number }>(
         where
             ur.user_id = :user_id
     `
+);
+
+/**
+ * Interface describing input parameters of findRolesByRoleName
+ *
+ * @export
+ * @interface IFindRolesByRoleName
+ */
+export interface IFindRolesByRoleName {
+    role: string | string[];
+}
+
+/**
+ * Finds role records by role names
+ */
+export const findRolesByRoleName = sql_query<ISysRole[], IFindRolesByRoleName>(
+    (params: IFindRolesByRoleName): IDynamicQuery => {
+        const role = wrapInArray(params.role);
+        return {
+            named: false,
+            sql: `select * from sys.role where role_name in (${role.map((a, i) => { return "$" + (i + 1); }).join(",")})`,
+            parameters: role
+        };
+    }
 );
