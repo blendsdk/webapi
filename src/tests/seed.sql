d386813788ada7c68b90e5136f4a3b248422906b
Gevik Babakhani <gevik@blendjs.com>
2019-07-27 19:19:11 +0400

    updated seed file.

--- src/tests/seed.sql
+++ src/tests/seed.sql
@@ -1,5 +1,11 @@
 insert into sys.user (username,password,email)
+values ('admin','$2y$12$TY3lMdKzS.JI308lBfQ9hOF8YCBPiXfyIahlCSbl96/bg6kyRtKtG','admin@example.com');
+
+insert into sys.user (username,password,email)
 values ('user1','$2y$12$TY3lMdKzS.JI308lBfQ9hOF8YCBPiXfyIahlCSbl96/bg6kyRtKtG','user1@example.com');
+
+insert into sys.role (role_name) values ('ADMIN');
+insert into sys.user_role (user_id,role_id) values(1,1);
 
 insert into sys.role (role_name) values ('role1');
 insert into sys.role (role_name) values ('role2');
