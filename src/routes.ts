3fc5df3861dddb2acc284be0ce64e61e8ca81fd0
Gevik Babakhani <gevik@blendjs.com>
2019-08-13 21:23:49 +0200

    added test route

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
-import { IRoute } from "@blendsdk/express";
+import { IRoute, response } from "@blendsdk/express";
+import { Request, Response } from "express";
 import { loginController } from "./controllers/authentication/loginController";
 
 const routes: IRoute[] = [
@@ -10,6 +11,20 @@ const routes: IRoute[] = [
         parameters: {
             username: { type: "string" },
             password: { type: "string" }
+        }
+    },
+    {
+        method: "post",
+        endpoint: "/api/params",
+        controller: async (req: Request, res: Response) => {
+            return response(res).OK({ ...req.body, ...req.query });
+        },
+        secure: false,
+        parameters: {
+            q: { type: "number", optional: true },
+            list: { type: "array", optional: true },
+            flag: { type: "boolean", optional: true },
+            amount: { type: "number", optional: true }
         }
     }
 ];
